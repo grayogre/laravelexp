ced88be7db1b6ff420ec34d086eea308a55c0e20
grayogre <grayogre@gmail.com>
2023-02-28 11:04:51 +0900

    DBユーザーsailに権限付け２

--- testgrant.sh
+++ testgrant.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mysql -u root -ppassword -e `ip -br -4 address show dev eth0 | awk -v FS="[ /]+" '{ print "GRANT ALL PRIVILEGES ON testing TO \"sail\"@\"" $3 "\";"}'`
+echo `ip -br -4 address show dev eth0 | awk -v FS="[ /]+" '{ print "GRANT ALL PRIVILEGES ON testing TO \"sail\"@\"" $3 "\";"}'`
